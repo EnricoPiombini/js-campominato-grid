8f2ffee49299d72d6e2f75fc95db077e2ac6db00
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-27 16:50:31 +0200

    cell-numbers

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
+// @ts-nocheck
 // Generare una griglia di gioco quadrata , in cui ogni cella contiene un numero incrementale tra quelli compresi tra 1 e 100
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 const gridContainer = document.querySelector(".gridContainer");
-
-// Creo una funzione per generare la griglia 6*6
+const gridNumbers = [];
+// Creo una funzione per generare la griglia 10*10
 
 function createGrid(Hcells, VCells) {
     const totalCells = Hcells * VCells;
     console.log(totalCells);
 
 
-
     //Uso un ciclo per creare le celle
 
-    for (let i = 0; i < totalCells; i++) {
+    for (let i = 1; i <= totalCells; i++) {
         // Creo un div con create element per rappresentare uan singola cella
         const cell = document.createElement('div');
         cell.classList.add("cell");
-        gridContainer.style.width= `width : calc(var(--cell-size))`;
+        gridContainer.style.width = `calc(var(--cell-size)* ${Hcells})`;
         gridContainer.append(cell);
-
+        cell.innerHTML = `<span>${i}</span>`
     }
 
+
+
+
 }
-createGrid(6, 6); 
+
+
+createGrid(10, 10); 
